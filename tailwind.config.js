0bf15467dfc78fa227da8298278365d9b12c8347
Severin Landolt <sev.landolt@gmail.com>
2023-05-08 23:45:36 +0200

    ideas

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,35 @@ module.exports = {
     current: "currentColor",
     "base-blue": "#1F7BE5",
     "hover-blue": "#005CC6",
-    extend: {},
+    extend: {
+      colors: {
+        bg: {
+          muted: 50, //cancasbackground
+          subtle: 100, // lightbackground
+          default: 500, //background
+          emphasis: 600, //darkbackground
+          strong: 800, //darkestbackground
+          inverted: 950, //new
+        },
+        border: {
+          muted: 100, // new
+          subtle: 200, //lightBorder
+          default: 500, //border
+          emphasis: 700, //darkborder
+        },
+        ring: {
+          subtle: 200, //lightring
+          default: 300, //ring
+        },
+        content: {
+          subtle: 400, //lighttext
+          default: 500, //text, //icon
+          emphasis: 700, //darktext
+          strong: 800, //darkesttext
+          inverted: WHITE, //new
+        },
+      },
+    },
   },
   plugins: [],
 };
