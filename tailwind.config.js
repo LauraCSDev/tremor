0e9c338d330b80aac8186cc175532eb0951ab293
Severin Landolt <sev.landolt@gmail.com>
2023-05-09 19:02:13 +0200

    update hex

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,36 +2,43 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    transparent: "transparent",
-    current: "currentColor",
-    "base-blue": "#1F7BE5",
-    "hover-blue": "#005CC6",
+    transparent: "transparent", // remove?
+    current: "currentColor", // remove?
+    "base-blue": "#1F7BE5", // remove?
+    "hover-blue": "#005CC6", // remove?
     extend: {
       colors: {
-        bg: {
-          muted: 50, //cancasbackground
-          subtle: 100, // lightbackground
-          default: 500, //background
-          emphasis: 600, //darkbackground
-          strong: 800, //darkestbackground
-          inverted: 950, //new
+        // primary: {
+        //   muted: "#bae6fd", //canvasbackground
+        //   subtle: "#7dd3fc", // lightbackground
+        //   default: "#3b82f6", // background
+        //   emphasis: "#1d4ed8", // darkbackground
+        //   inverted: "#eff6ff", // new
+        // },
+        background: {
+          muted: "#f9fafb", //canvasbackground
+          subtle: "#f3f4f6", // lightbackground
+          default: "#ffffff", // background
+          emphasis: "#4b5563", // darkbackground
+          strong: "#1f2937", // darkestbackground
+          inverted: "#030712", // new
         },
         border: {
-          muted: 100, // new
-          subtle: 200, //lightBorder
-          default: 500, //border
-          emphasis: 700, //darkborder
+          muted: "#f3f4f6", // new
+          subtle: "#e5e7eb", //lightBorder
+          default: "#6b7280", //border
+          emphasis: "#374151", //darkborder
         },
         ring: {
-          subtle: 200, //lightring
-          default: 300, //ring
+          subtle: "#e5e7eb", //lightring
+          default: "#d1d5db", //ring
         },
         content: {
-          subtle: 400, //lighttext
-          default: 500, //text, //icon
-          emphasis: 700, //darktext
-          strong: 800, //darkesttext
-          inverted: WHITE, //new
+          subtle: "#9ca3af", //lighttext
+          default: "#6b7280", //text, //icon
+          emphasis: "#374151", //darktext
+          strong: "#1f2937", //darkesttext
+          inverted: "#ffffff", //new
         },
       },
     },
