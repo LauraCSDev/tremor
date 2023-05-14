d3b7999ce189765cea14f21974920c0c55a3cb6c
Severin Landolt <sev.landolt@gmail.com>
2023-05-14 22:31:06 +0200

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,70 +4,57 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // primary: {
-        //   muted: "#bae6fd", //canvasbackground
-        //   subtle: "#7dd3fc", // lightbackground
-        //   default: "#3b82f6", // background
-        //   emphasis: "#1d4ed8", // darkbackground
-        //   inverted: "#eff6ff", // new
-        // },
-        // background: {
-        //   muted: "#f9fafb", //canvasbackground 50
-        //   subtle: "#f3f4f6", // lightbackground 100
-        //   default: "#ffffff", // background
-        //   emphasis: "#4b5563", // darkbackground
-        //   strong: "#1f2937", // darkestbackground
-        //   inverted: "#030712", // new
-        // },
-        // border: {
-        //   muted: "#f3f4f6", // new 100
-        //   subtle: "#e5e7eb", //lightBorder
-        //   default: "#6b7280", //border
-        //   emphasis: "#374151", //darkborder
-        // },
-        // ring: {
-        //   subtle: "#e5e7eb", //lightring
-        //   default: "#d1d5db", //ring
-        // },
-        // content: {
-        //   subtle: "#9ca3af", //lighttext
-        //   default: "#6b7280", //text, //icon
-        //   emphasis: "#374151", //darktext
-        //   strong: "#1f2937", //darkesttext
-        //   inverted: "#ffffff", //new
-        // },
         tremor: {
-          muted: "#f9fafb",
-          subtle: "#f3f4f6",
-          default: "#ffffff",
-          emphasis: "#4b5563",
-          strong: "#1f2937",
-          inverted: "#030712",
+          brand: {
+            muted: "#bae6fd", // disabled
+            subtle: "#7dd3fc", // 300 zb hover
+            DEFAULT: "#3b82f6", // 500
+            emphasis: "#1d4ed8", // 700
+            inverted: "#eff6ff", //
+          },
+          background: {
+            muted: "#f9fafb", //canvasbackground 50
+            subtle: "#f3f4f6", // lightbackground 100
+            DEFAULT: "#ffffff", // background
+            emphasis: "#4b5563", // darkbackground
+            strong: "#1f2937", // darkestbackground
+            inverted: "#030712", // new
+          },
+          border: {
+            muted: "#f3f4f6", // new 100
+            subtle: "#e5e7eb", //lightBorder
+            DEFAULT: "#6b7280", //border
+            emphasis: "#374151", //darkborder
+          },
+          ring: {
+            subtle: "#e5e7eb", //lightring
+            DEFAULT: "#d1d5db", //ring
+          },
+          content: {
+            subtle: "#9ca3af", //lighttext
+            DEFAULT: "#6b7280", //text, //icon
+            emphasis: "#374151", //darktext
+            strong: "#1f2937", //darkesttext
+            inverted: "#ffffff", //new
+          },
+          shadow: {
+            sm: "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+            DEFAULT: "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+            md: "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+            lg: "0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)",
+          },
+          rounded: {
+            sm: "0.125rem",
+            DEFAULT: "0.25rem",
+          },
+          fontSize: {
+            DEFAULT: ["1rem", { lineHeight: "1.5rem" }],
+          },
+          fontWeight: {
+            default: "400",
+            emphasis: "500",
+          },
         },
-        // baseColor: {
-        //   slate: "#64748b",
-        //   gray: "#6b7280",
-        //   zinc: "#71717a",
-        //   neutral: "#737373",
-        //   stone: "#78716c",
-        //   red: "#ef4444",
-        //   orange: "#f97316",
-        //   amber: "#f59e0b",
-        //   yellow: "#eab308",
-        //   lime: "#84cc16",
-        //   green: "#22c55e",
-        //   emerald: "#10b981",
-        //   teal: "#14b8a6",
-        //   cyan: "#06b6d4",
-        //   sky: "#0ea5e9",
-        //   blue: "#3b82f6",
-        //   indigo: "#6366f1",
-        //   violet: "#8b5cf6",
-        //   purple: "#a855f7",
-        //   fuchsia: "#d946ef",
-        //   pink: "#ec4899",
-        //   rose: "#f43f5e",
-        // },
       },
     },
   },
