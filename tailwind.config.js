19ec4ad125264447795532211ce01de75f803855
Severin Landolt <sev.landolt@gmail.com>
2023-05-14 17:52:48 +0200

    added variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    transparent: "transparent", // remove?
-    current: "currentColor", // remove?
-    "base-blue": "#1F7BE5", // remove?
-    "hover-blue": "#005CC6", // remove?
     extend: {
       colors: {
         // primary: {
@@ -15,31 +11,63 @@ module.exports = {
         //   emphasis: "#1d4ed8", // darkbackground
         //   inverted: "#eff6ff", // new
         // },
-        background: {
-          muted: "#f9fafb", //canvasbackground
-          subtle: "#f3f4f6", // lightbackground
-          default: "#ffffff", // background
-          emphasis: "#4b5563", // darkbackground
-          strong: "#1f2937", // darkestbackground
-          inverted: "#030712", // new
+        // background: {
+        //   muted: "#f9fafb", //canvasbackground 50
+        //   subtle: "#f3f4f6", // lightbackground 100
+        //   default: "#ffffff", // background
+        //   emphasis: "#4b5563", // darkbackground
+        //   strong: "#1f2937", // darkestbackground
+        //   inverted: "#030712", // new
+        // },
+        // border: {
+        //   muted: "#f3f4f6", // new 100
+        //   subtle: "#e5e7eb", //lightBorder
+        //   default: "#6b7280", //border
+        //   emphasis: "#374151", //darkborder
+        // },
+        // ring: {
+        //   subtle: "#e5e7eb", //lightring
+        //   default: "#d1d5db", //ring
+        // },
+        // content: {
+        //   subtle: "#9ca3af", //lighttext
+        //   default: "#6b7280", //text, //icon
+        //   emphasis: "#374151", //darktext
+        //   strong: "#1f2937", //darkesttext
+        //   inverted: "#ffffff", //new
+        // },
+        tremor: {
+          muted: "#f9fafb",
+          subtle: "#f3f4f6",
+          default: "#ffffff",
+          emphasis: "#4b5563",
+          strong: "#1f2937",
+          inverted: "#030712",
         },
-        border: {
-          muted: "#f3f4f6", // new
-          subtle: "#e5e7eb", //lightBorder
-          default: "#6b7280", //border
-          emphasis: "#374151", //darkborder
-        },
-        ring: {
-          subtle: "#e5e7eb", //lightring
-          default: "#d1d5db", //ring
-        },
-        content: {
-          subtle: "#9ca3af", //lighttext
-          default: "#6b7280", //text, //icon
-          emphasis: "#374151", //darktext
-          strong: "#1f2937", //darkesttext
-          inverted: "#ffffff", //new
-        },
+        // baseColor: {
+        //   slate: "#64748b",
+        //   gray: "#6b7280",
+        //   zinc: "#71717a",
+        //   neutral: "#737373",
+        //   stone: "#78716c",
+        //   red: "#ef4444",
+        //   orange: "#f97316",
+        //   amber: "#f59e0b",
+        //   yellow: "#eab308",
+        //   lime: "#84cc16",
+        //   green: "#22c55e",
+        //   emerald: "#10b981",
+        //   teal: "#14b8a6",
+        //   cyan: "#06b6d4",
+        //   sky: "#0ea5e9",
+        //   blue: "#3b82f6",
+        //   indigo: "#6366f1",
+        //   violet: "#8b5cf6",
+        //   purple: "#a855f7",
+        //   fuchsia: "#d946ef",
+        //   pink: "#ec4899",
+        //   rose: "#f43f5e",
+        // },
       },
     },
   },
